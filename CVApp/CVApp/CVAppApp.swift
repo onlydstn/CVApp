6648ac035d1ac85ecd5d145055eb6df7a9c9b9d5
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-08-06 14:40:22 +0200

    initial commit

---
+++ CVApp/CVApp/CVAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVAppApp.swift
+//  CVApp
+//
+//  Created by Dustin Nuzzo on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CVAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CVView()
+        }
+    }
+}
---
+++ CVApp/Views/CVView.swift
@@ -0,0 +1,18 @@
+//
+//  CVView.swift
+//  CVApp
+//
+//  Created by Dustin Nuzzo on 8/6/24.
+//
+
+import SwiftUI
+
+struct CVView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CVView()
+}
