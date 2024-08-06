c89d107b7aaf90e31e9e10fa69aa7cb3fe676cea
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-08-07 00:41:34 +0200

    UI verbessert, Profilbild hinzugefügt ToDo: Icons und RoundedRectangle hinzufügen

--- CVApp/Views/HeaderView.swift
+++ CVApp/Views/HeaderView.swift
@@ -10,36 +10,61 @@ import SwiftUI
 struct HeaderView: View {
     
     private let infos = UserInfo(name: "Dustin Nuzzo", age: 29, birthDate: "18th February 1995", phoneNumber: "+491711180295", address: "Duisburg, Germany", email: "dustin.nuzzo@icloud.com")
-
-        
+    
+    
     var body: some View {
-        VStack {
+        ZStack {
+            Color(red: 0.95, green: 0.95, blue: 0.95)
+                .edgesIgnoringSafeArea(.all)
             VStack {
-                Text("🙋🏽‍♂️")
-                    .font(.largeTitle)
-                Text(infos.name)
-                    .font(.title)
-                    .bold()
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(Color(red: 0.3, green: 0.56, blue: 0.8))
+                    .frame(height: 135)
+                    .edgesIgnoringSafeArea(.top)
+                
+                Spacer()
             }
-            .background(.brown)
-            .padding(5)
             VStack {
-                Text("Learning Mobile Development \n at Syntax Institut")
-                    .multilineTextAlignment(.center)
+                VStack {
+                    Image("profilePicture")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 115, height: 115)
+                        .clipShape(Circle())
+                        .shadow(radius: 10)
+                    Text(infos.name)
+                        .font(.title2)
+                        .bold()
+                }
+                //.background(.brown)
+                .padding(1)
+                VStack {
+                    Text("Learning Mobile Development \n at Syntax Institut")
+                        .multilineTextAlignment(.center)
+                        .font(.footnote)
+                }
+                //.background(.red)
+                .padding(1)
+                HStack(spacing: 130) {
+                    Text(infos.address)
+                        .font(.footnote)
+                    Text(infos.phoneNumber)
+                        .font(.footnote)
+                }
+                //.background(.yellow)
+                .padding(5)
+                HStack(spacing: 70) {
+                    Text(infos.email)
+                        .font(.footnote)
+                    Text(infos.birthDate)
+                        .font(.footnote)
+                }
+                //.background(.purple)
+                Divider()
+                Spacer()
             }
-            .background(.red)
-            .padding(5)
-            HStack {
-                Text(infos.birthDate)
-                Text(infos.email)
-            }
-            .background(.yellow)
-            HStack(spacing: 100) {
-                Text(infos.address)
-                Text(infos.phoneNumber)
-            }
-            .background(.purple)
+            //.background(.green)
+            .padding(.top)
         }
-        .background(.green)
     }
 }
