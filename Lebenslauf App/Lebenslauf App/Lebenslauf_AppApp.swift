49b918300c9ae01806dc3e270afe119b7074964c
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-08-06 12:16:01 +0200

    initial commit

---
+++ Lebenslauf App/Lebenslauf App/Lebenslauf_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lebenslauf_AppApp.swift
+//  Lebenslauf App
+//
+//  Created by Dustin Nuzzo on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Lebenslauf_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
